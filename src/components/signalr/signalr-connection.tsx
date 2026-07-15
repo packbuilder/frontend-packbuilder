f704fe0c05d24f749159ade2ad956583d81f4844
Goob-dev <solizg02@gmail.com>
2026-07-15 12:51:55 -0500

    Adding logs for testing

--- src/components/signalr/signalr-connection.tsx
+++ src/components/signalr/signalr-connection.tsx
@@ -1,6 +1,8 @@
 import { getUserToken } from "@/lib/api";
 import * as signalR from "@microsoft/signalr"
 
+console.log(import.meta.env.VITE_APIURL);
+
 const VITE_APIURL = import.meta.env.VITE_APIURL ? import.meta.env.VITE_APIURL : "/api";
 export const connection = new signalR.HubConnectionBuilder()
     .withUrl(`${VITE_APIURL}/hubs/modpacks`, {accessTokenFactory: () => getUserToken() || ""})
