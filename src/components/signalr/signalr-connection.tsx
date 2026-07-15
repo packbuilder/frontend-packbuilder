b9e0bfb1e9f5b7805a80d5397c155d0b06f4a707
Goob-dev <solizg02@gmail.com>
2026-07-15 13:28:25 -0500

    Updated docker ingore to not ignore env production so it builds with it.

--- src/components/signalr/signalr-connection.tsx
+++ src/components/signalr/signalr-connection.tsx
@@ -1,9 +1,7 @@
 import { getUserToken } from "@/lib/api";
 import * as signalR from "@microsoft/signalr"
 
-console.log(import.meta.env.VITE_APIURL);
-
-const VITE_APIURL = import.meta.env.VITE_APIURL ? import.meta.env.VITE_APIURL : "/api";
+const VITE_APIURL = import.meta.env.VITE_APIURL;
 export const connection = new signalR.HubConnectionBuilder()
     .withUrl(`${VITE_APIURL}/hubs/modpacks`, {accessTokenFactory: () => getUserToken() || ""})
     .withAutomaticReconnect([0, 2000, 5000, 10000])
